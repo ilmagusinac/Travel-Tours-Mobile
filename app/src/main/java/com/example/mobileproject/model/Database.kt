4572fb231ebe2146cc27718b959f190f0fc8fd50
jeftovica <119257123+jeftovica@users.noreply.github.com>
2024-05-28 15:48:49 +0200

    Update Database.kt

--- app/src/main/java/com/example/mobileproject/model/Database.kt
+++ app/src/main/java/com/example/mobileproject/model/Database.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+
 import com.example.mobileproject.model.daos.UsersDao
 import com.example.mobileproject.model.daos.ToursDao
 import com.example.mobileproject.model.daos.AttractionsDao
